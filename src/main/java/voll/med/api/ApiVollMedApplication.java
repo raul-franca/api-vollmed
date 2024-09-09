bac5be18f53886df0581273db721d6194668727c
Raul Franca <raulmfranca@gmail.com>
2024-09-09 12:31:40 -0300

    :wrench: build: Update database configs

--- src/main/java/voll/med/api/ApiVollMedApplication.java
+++ src/main/java/voll/med/api/ApiVollMedApplication.java
@@ -1,54 +1,25 @@
 package voll.med.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.Map;
-
 @SpringBootApplication
 @Controller
 public class ApiVollMedApplication {
-    private final DataSource dataSource;
-
-    @Autowired
-    public ApiVollMedApplication(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
-    @GetMapping("/database")
-    String database(Map<String, Object> model) {
-        try (Connection connection = dataSource.getConnection()) {
-            final var statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
-
-            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
-            final var output = new ArrayList<>();
-            while (resultSet.next()) {
-                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
-            }
-
-            model.put("records", output);
-            return "database";
-
-        } catch (Throwable t) {
-            model.put("message", t.getMessage());
-            return "error";
-        }
-    }
-
     public static void main(String[] args) {
+         // Carregar variáveis do arquivo .env
+        Dotenv dotenv = Dotenv.load();
+        // Verifique se a variável DATABASE_URL está sendo carregada
+        System.out.println("DATABASE_URL: " + dotenv.get("DATABASE_URL"));
         SpringApplication.run(ApiVollMedApplication.class, args);
     }
 }
