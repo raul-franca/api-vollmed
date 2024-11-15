79ca63c97c6936ec94c33c0f02a8079fbef30afa
Raul Franca <raulmfranca@gmail.com>
2024-11-15 15:19:32 -0300

    :bulb: add comentários

--- src/main/java/voll/med/api/infra/TratadorDeErros.java
+++ src/main/java/voll/med/api/infra/TratadorDeErros.java
@@ -9,17 +9,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
+// Anotação que indica que esta classe fornecerá tratamento global de exceções para controladores REST
 @RestControllerAdvice
 public class TratadorDeErros {
 
+    // Metodo que trata exceções do tipo EntityNotFoundException
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Void> tratarErro404() {
+        // Retorna uma resposta HTTP 404 (Not Found) sem corpo
         return ResponseEntity.notFound().build();
     }
 
+    // Metodo que trata exceções do tipo MethodArgumentNotValidException (erros de validação de campos)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List> tratarErro400(MethodArgumentNotValidException ex) {
+    public ResponseEntity<List<DadosErroValidacao>> tratarErro400(MethodArgumentNotValidException ex) {
+        // Obtém a lista de erros de validação dos campos
         var erros = ex.getFieldErrors();
+        // Mapeia cada erro para o record DadosErroValidacao e retorna uma resposta HTTP 400 (Bad Request) com a lista de erros no corpo
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
     }
 
