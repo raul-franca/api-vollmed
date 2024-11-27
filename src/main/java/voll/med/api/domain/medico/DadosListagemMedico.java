713cab1bac1a9922121c105a83badc2fe05576d8
Raul Franca <raulmfranca@gmail.com>
2024-11-27 16:47:58 -0300

    :art: add telefone na listagem de medicos

--- src/main/java/voll/med/api/domain/medico/DadosListagemMedico.java
+++ src/main/java/voll/med/api/domain/medico/DadosListagemMedico.java
@@ -1,9 +1,9 @@
 package voll.med.api.domain.medico;
 
-public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedico(Long id, String nome, String email, String telefone, String crm, Especialidade especialidade) {
 
 
     public DadosListagemMedico(Medico medico) {
-        this(medico.getId(),  medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(),  medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade());
     }
 }
