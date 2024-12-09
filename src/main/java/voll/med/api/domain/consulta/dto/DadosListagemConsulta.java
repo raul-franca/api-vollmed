2b97ab611ec78a1d3ebf577e416e587ffe698e14
Raul Franca <raulmfranca@gmail.com>
2024-12-09 19:06:00 -0300

    :sparkles: add crm e telefone do paciente no DadosListagemConsulta

--- src/main/java/voll/med/api/domain/consulta/dto/DadosListagemConsulta.java
+++ src/main/java/voll/med/api/domain/consulta/dto/DadosListagemConsulta.java
@@ -6,10 +6,16 @@ import voll.med.api.domain.medico.Especialidade;
 
 import java.time.LocalDateTime;
 
-public record DadosListagemConsulta(Long id, LocalDateTime data, Especialidade especialidade, String medico, String paciente) {
+public record DadosListagemConsulta(Long id, LocalDateTime data, String crm, Especialidade especialidade, String medico, String paciente, String telefone) {
 
     public DadosListagemConsulta(Consulta consulta) {
-        this(consulta.getId(), consulta.getData(), consulta.getMedico().getEspecialidade(), consulta.getMedico().getNome(), consulta.getPaciente().getNome());
+        this(consulta.getId(),
+                consulta.getData(),
+                consulta.getMedico().getCrm(),
+                consulta.getMedico().getEspecialidade(),
+                consulta.getMedico().getNome(),
+                consulta.getPaciente().getNome()
+                ,consulta.getPaciente().getTelefone());
     }
 
 }
